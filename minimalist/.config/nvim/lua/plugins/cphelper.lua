b965abeea6fe4634237a20c5846321a38d4cd0f4
mrunknown850 <minh0608@outlook.com>
2025-07-12 22:22:58 +0700

    add some plugins for nvim and refactor directories
    Plugins added:
    - conform (formatter)
    - cphelper (competitive programming)
    - dap (debugger)
    - lsp (language server)
    - mdpv (previewing markdowns)
    - treesitter (colours)

---
+++ minimalist/.config/nvim/lua/plugins/cphelper.lua
@@ -0,0 +1,7 @@
+return {
+    "p00f/cphelper.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+  }
+
+
---
+++ minimalist/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    opts = function()
+      local dap = require("dap")
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb",
+          args = { "--port", "${port}" },
+        },
+      }
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+    end,
+  },
+}
