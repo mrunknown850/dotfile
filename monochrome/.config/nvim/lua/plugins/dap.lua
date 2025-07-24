70bb648e3488ef12af2cd5b730938c5122d12ffc
mrunknown850 <minh0608@outlook.com>
2025-07-24 16:36:17 +0700

    create new stow

---
+++ monochrome/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "jay-babu/mason-nvim-dap.nvim",
+    },
+    opts = require "configs.dap",
+  },
+}
---
+++ monochrome/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  require("plugins.dap"),
+  require("plugins.formatting"),
+  require("plugins.cphelper"),
+  require("plugins.markdown-preview"),
+  require("plugins.lsp"),
+  require("plugins.treesitter"),
+}
